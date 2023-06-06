55404f62ae63c3ea2387d38a7b2b9eb3c7f70af3
Diego Cepeda <Diegocepar@gmail.com>
2023-06-05 19:55:35 -0700

    update changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -9,34 +9,60 @@ day - the day of the week
 desc - a description of the coffee
 
 */
-let myDate= new Date();
+let myDate = new Date();
 
 let today = myDate.getDay();
 
-//today= 3;
+let coffee = "";
+
+function coffeeTemplate(coffee){
+
+     return `
+     <p>
+             <img src="${coffee.pic}" alt="${coffee.alt}" id = "coffee"/>
+             <strong class="feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special is <strong 
+             class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
+     `;
+ 
+ }
+//test today = 3;
 
 switch(today){
 
-   case 2:
-        today="Tuesday";
-   break;
-   case 3:
-        today="Wednesday";
-   break;
+    case 1:
+        today = "Monday";
 
-   default:
-       today="Day is unknown";
+        coffee = {
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            alt: "A picture of a bubble tea",
+            color: "pink",
+            day: "Monday",
+            desc: `I love me some bubble tea!`
+        };
+    break;
+
+    case 2:
+        today = "Tuesday";
+    break;
+
+    case 3:
+        today = "Wednesday";
+    break;
+
+    default:
+        today ="Day is unknown!";
 }
 
+coffee = {
+    name: "Bubble Tea",
+    pic: "images/bubble-tea.jpg",
+    alt: "A picture of a bubble tea",
+    color: "pink",
+    day: "Wednesday",
+    desc: `I love me some bubble tea!`
+}
 
-
-let coffee = {
-     name: "Bubble Tea",
-     pic: "images/bubble-tea.jpg",
-     alt: "A picture of a bubble tea",
-     color: "blue",
-     day: "Wednesday",
-     desc: 'I Love me some bubble tea!'
- }
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
 
 console.log(coffee);

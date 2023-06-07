74a20563d7fe72c1b03f8f919dfdfb5786037b23
Diego Cepeda <Diegocepar@gmail.com>
2023-06-07 16:22:49 -0700

    update changes

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -15,6 +15,17 @@ let today = myDate.getDay();
 
 let coffee = "";
 
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+
+
+
 function coffeeTemplate(coffee){
 
      return `
@@ -27,7 +38,26 @@ function coffeeTemplate(coffee){
  }
 //test today = 3;
 
+if(urlParams.has("day")){//from querystring
+    today = urlParams.get("day");
+ }
+
+ today = parseInt(today);
+
 switch(today){
+
+    case 0:
+        today = "Sunday";
+
+        coffee = {
+            name: "Caramel-latte",
+            pic: "images/caramel-latte.jpg",
+            alt: "A picture of a caramel latte",
+            color: "yellow",
+            day: "Sunday",
+            desc: `Caramel-latte contains espresso, frothed milk, and caramel sauce`
+        };
+    break;
 
     case 1:
         today = "Monday";
@@ -38,31 +68,82 @@ switch(today){
             alt: "A picture of a bubble tea",
             color: "pink",
             day: "Monday",
-            desc: `I love me some bubble tea!`
+            desc: `Bubble tea contains black tea, milk, ice, and chewy tapioca pearls`
         };
     break;
 
     case 2:
         today = "Tuesday";
-    break;
 
+        coffee = {
+            name: "Mocha",
+            pic: "images/mocha.jpg",
+            alt: "A picture of a mocha",
+            color: "brown",
+            day: "Tuesday",
+            desc: `Mocha  a shot of espresso is combined with a chocolate powder or syrup, followed by milk or cream`
+        };
+
+    break;
     case 3:
         today = "Wednesday";
-    break;
 
+        coffee = {
+            name: "Frappaccino",
+            pic: "images/frappaccino.jpg",
+            alt: "A picture of a frappaccino",
+            color: "black",
+            day: "wednesday",
+            desc: `A frappaccino contains a line of iced, blended coffee with whipped cream and caramel syrup. `
+        };
+
+    break;
+    case 4:
+        today = "Thursday";
+
+        coffee = {
+            name: "Pumpkin-spice-latte",
+            pic: "images/Pumpkin-spice-latte.jpg",
+            alt: "A picture of a Pumpkin-spice-latte",
+            color: "blue",
+            day: "thursday",
+            desc: `A  Pumpkin-spice-latte contains a combination of pumpkin, cinnamon, nutmeg and clove`
+        };
+
+    break;
+    case 5:
+        today = "Friday";
+
+        coffee = {
+            name: "drip",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip",
+            color: "green",
+            day: "friday",
+            desc: `We filtering hot water over ground coffee`
+        };
+
+    break;
+    case 6:
+        today = "Saturday";
+
+        coffee = {
+            name: "cold-brew",
+            pic: "images/cold-brew.jpg",
+            alt: "A picture of a cold-brew",
+            color: "red",
+            day: "saturday",
+            desc: `A cold coffee!`
+        };
+
+    break;
     default:
         today ="Day is unknown!";
 }
 
-coffee = {
-    name: "Bubble Tea",
-    pic: "images/bubble-tea.jpg",
-    alt: "A picture of a bubble tea",
-    color: "pink",
-    day: "Wednesday",
-    desc: `I love me some bubble tea!`
-}
 
 document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+document.querySelector("html").style.backgroundColor = coffee.color;
 
 console.log(coffee);
